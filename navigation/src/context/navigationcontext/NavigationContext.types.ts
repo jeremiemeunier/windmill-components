f700de880cb47c3d341d5d555fde60f48c95449f
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-03-17 15:26:59 +0100

    add navigation

---
+++ navigation/src/context/navigationcontext/NavigationContext.types.ts
@@ -0,0 +1,16 @@
+import { SetStateAction } from "react";
+
+export interface ContextProps {
+  children: React.ReactNode;
+}
+
+export interface ProviderProps {
+  appActualPage: string;
+  setAppActualPage: React.Dispatch<SetStateAction<string>>;
+  appActualSubPage: string;
+  setAppActualSubPage: React.Dispatch<SetStateAction<string>>;
+  appActualCategory: string;
+  setAppActualCategory: React.Dispatch<SetStateAction<string>>;
+  appMenuVisibility: boolean;
+  setAppMenuVisibility: React.Dispatch<SetStateAction<boolean>>;
+}
