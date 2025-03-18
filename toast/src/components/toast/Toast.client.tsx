6fc2a4de44b8544c21bb23094ef84e1069f04932
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-03-18 08:50:49 +0100

    fixing

--- toast/src/components/toast/Toast.client.tsx
+++
@@ -1,2 +0,0 @@
-"use client";
-export { default as Toast } from "./Toast";
--- toast/src/components/toast/Toast.tsx
+++ toast/src/components/toast/Toast.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { Loader } from "@jeremiemeunier/windmill-core";
 import { TimerProps, ToastProps } from "./Toast.types";
