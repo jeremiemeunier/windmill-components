b4bdeae7a370d637d1ac10a5bb5032c3fc2d7a40
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-11-06 10:47:42 +0100

    Add Adress autocomplete component
    Introduces a new Adress component with autocomplete functionality, including its type definitions and index export. The component supports displaying suggestions, handling selection, and various input states such as loading, disabled, and read-only.

---
+++ xform/src/components/adress/Adress.types.ts
@@ -0,0 +1,39 @@
+export interface AdressItems {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: number[];
+  };
+  properties: {
+    label: string;
+    score: number;
+    housenumber: string;
+    id: string;
+    banId: string;
+    name: string;
+    postcode: string;
+    citycode: string;
+    x: number;
+    y: number;
+    city: string;
+    context: string;
+    type: string;
+    importance: number;
+    street: string;
+  };
+}
+
+export interface AdressProps {
+  name: string;
+  error: string | undefined;
+  data: AdressItems[] | [];
+  label?: string;
+  disabled?: boolean;
+  size?: number;
+  readOnly?: boolean;
+  maxLength?: number;
+  placeHolder?: string;
+  required?: boolean;
+  className?: string;
+  dataIsLoading?: boolean;
+}
